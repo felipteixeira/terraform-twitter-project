2f17a65bf79391d9db0c50537a8c9635b371939d
Felipe Teixeira <felipe.teixeira@willbank.com.br>
2021-03-28 12:51:39 -0300

    sg edit

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -29,14 +29,6 @@ resource "aws_security_group" "ecs_main_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Public access"
-  }
-
-  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "TCP"
