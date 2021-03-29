9592b76c6b990549378586b740ac2cfbce882656
Felipe Teixeira <felipe.teixeira@willbank.com.br>
2021-03-28 22:04:06 -0300

    ecr edit

--- modules/ecs-ec2/ecr.tf
+++ modules/ecs-ec2/ecr.tf
@@ -7,22 +7,6 @@ resource "aws_ecr_repository" "ecr_service" {
   }
 }
 
-# data "template_file" "ecr_lifecycle_policy" {
-#   template = file("./policies/ecr-lifecycle-policy.json")
-
-#   vars = {
-#     count_unit   = var.ecr_lifecycle_unit
-#     count_number = var.ecr_lifecycle_number
-#   }
-# }
-
-# resource "aws_ecr_lifecycle_policy" "ecr_service_policy" {
-#   repository = aws_ecr_repository.ecr_service.name
-#   policy     = data.template_file.ecr_lifecycle_policy.rendered
-# }
-
-#Caso seja necessário adicionar um politica customizada
-#Exemplo: Permitir outra conta fazer ações nesse repositório.
 resource "aws_ecr_repository_policy" "ecr_custom_policy" {
   count      = var.ecr_custom_policy != "" ? 1 : 0
   repository = aws_ecr_repository.ecr_service[count.index].name
